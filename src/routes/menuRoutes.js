72b5d369eac763e027c1b1084ea63e6ddd4082ed
Jake <jakefabz@gmail.com>
2026-04-28 16:22:46 -0400

    Implementation of menuRoutes.js

---
+++ src/routes/menuRoutes.js
@@ -0,0 +1,11 @@
+import {getMenuItemsHandler, getMenuItemByIdHandler,createMenuItemHandler, removeMenuItemHandler, updateMenuItemHandler } from '../controllers/menuController.js'; 
+import express from 'express'; 
+import {authorizeRoles} from '../middleware/authorizeRoles.js';
+
+const router = express.Router(); 
+
+router.get('/', getMenuItemsHandler, authorizeRoles('admin', 'user')); 
+router.get('/:id', getMenuItemByIdHandler);
+router.post('/', createMenuItemHandler, authorizeRoles('admin')); 
+router.put('/:id', updateMenuItemHandler, authorizeRoles('admin')); 
+router.delete('/:id', removeMenuItemHandler, authorizeRoles('admin')); 
